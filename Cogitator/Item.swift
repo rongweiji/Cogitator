d5df568035fb3ed2b66f1a59e67b89e461d3b44c
wayne <lycazure@hotmail.com>
2025-12-06 17:19:19 -0800

    Initial Commit

---
+++ Cogitator/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Cogitator
+//
+//  Created by Rongwei Ji on 12/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CogitatorTests/CogitatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CogitatorTests.swift
+//  CogitatorTests
+//
+//  Created by Rongwei Ji on 12/6/25.
+//
+
+import Testing
+@testable import Cogitator
+
+struct CogitatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
